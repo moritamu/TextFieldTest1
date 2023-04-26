6992daa552ddd1ffa05a5172a09845e80155ea4a
morisum <moris@r.sannet.ne.jp>
2023-04-27 06:23:03 +0900

    Initial Commit

---
+++ TextFieldTest1/TextFieldTest1App.swift
@@ -0,0 +1,17 @@
+//
+//  TextFieldTest1App.swift
+//  TextFieldTest1
+//
+//  Created by MsMacM on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct TextFieldTest1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
