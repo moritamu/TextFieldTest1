c01b95142d02b2b01c46b44672d529b2985bb3a3
morisum <moris@r.sannet.ne.jp>
2023-04-27 06:48:55 +0900

    まず最初、TextFieldを使った

--- TextFieldTest1/ContentView.swift
+++ TextFieldTest1/ContentView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var name = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("あなたの名前", text: $name)
+                .padding()
+            Text("こんにちは、\(name)さん")
         }
         .padding()
     }
